cd5279476118c38bfb928e30f55a67bed29091cb
Priyanshu Yadav <114854534+ipriyanshu25@users.noreply.github.com>
2026-06-18 16:26:12 +0530

    update

--- routes/Invitationsroutes.js
+++ routes/Invitationsroutes.js
@@ -1,6 +1,6 @@
-// routes/invitationRoutes.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+
 const {
   createInvitation,
   updateInvitationStatus,
@@ -8,13 +8,13 @@ const {
   getInvitationList,
   getInvitationSendEligibility,
   sendInvitationFollowUp,
-} = require('../controllers/NewInvitationsController');
+} = require("../controllers/NewInvitationsController");
 
-router.post('/create', createInvitation);
-router.post('/update', updateInvitationStatus);
-router.post('/list', listInvitations);
-router.post('/getList', getInvitationList);
-router.post('/eligibility', getInvitationSendEligibility);
+router.post("/create", createInvitation);
+router.post("/update", updateInvitationStatus);
+router.post("/list", listInvitations);
+router.post("/getList", getInvitationList);
+router.post("/eligibility", getInvitationSendEligibility);
 router.post("/followup", sendInvitationFollowUp);
 
 module.exports = router;
